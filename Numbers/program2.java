f44fc4fa4e686a1d2d0929c766cfedc9e410a74c
AbdullahNiyas24 <abdullahniyas264@gmail.com>
2026-02-07 14:20:46 +0530

    Numbers

---
+++ Numbers/program2.java
@@ -0,0 +1,17 @@
+package Numbers;
+import java.util.Scanner;
+public class program2 {
+	    public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+	        System.out.print("Enter a number: ");
+	        int num = sc.nextInt();
+	        int sum = 0;
+	        int digit;
+	        while (num > 0) {
+	            digit = num % 10;   
+	            sum = sum + digit; 
+	            num = num / 10;    
+	        }
+	        System.out.println("Sum of digits = " + sum);
+	    }
+	}
